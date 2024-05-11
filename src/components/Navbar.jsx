dc28569f3855e47109ccc901e6c193a816e104fa
shahoriarniloy <niloyshahoriar@gmail.com>
2024-05-11 19:21:11 +0600

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+                        </svg>
+                    </div>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><a>Home</a></li>
+                        <li><a>All Food</a></li>
+                        <li><a>Gallery</a></li>
+                    </ul>
+                </div>
+                <div className='h-16 w-16 rounded-full overflow-hidden'>
+    <img src="https://i.ibb.co/f8qWJY4/OIG4-m-M0lvw5e-Lb-Jt.jpgRestoSync" alt="RestoSync" className="h-full w-full object-cover" />
+</div>
+
+              </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <li><a>Home</a></li>
+                    <li><a>All Food</a></li>
+                    <li><a>Gallery</a></li>
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <div className="flex-none">
+                    <div className="dropdown dropdown-end">
+                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                            <div className="w-16 rounded-full">
+                                <img alt="Profile" src="https://i.ibb.co/v1qmfRn/836.jpg" />
+                            </div>
+                        </div>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            <li>
+                                <a className="justify-between">
+                                    Profile
+                                    <span className="badge">New</span>
+                                </a>
+                            </li>
+                            <li><a>Settings</a></li>
+                            <li><a>Logout</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
